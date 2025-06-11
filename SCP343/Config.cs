872d0c17cddcc538eda8b1174d2ba8a0f9c1ad67
zazarick <danyamalyshev2023@gmail.com>
2025-06-11 16:28:32 +0300

    Update Config.cs

--- SCP343/Config.cs
+++ SCP343/Config.cs
@@ -1,48 +1,53 @@
-using System.ComponentModel;
 using Exiled.API.Interfaces;
 using PlayerRoles;
-using Exiled.API.Features.Items;
+using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Scp343
 {
     public class Scp343Config : IConfig
     {
-        [Description("Включён ли плагин SCP-343?")]
+        [Description("Включить ли плагин?")]
         public bool IsEnabled { get; set; } = true;
 
-        [Description("Включить отладочное логирование?")]
+        [Description("Включить режим отладки?")]
         public bool Debug { get; set; } = false;
 
-        [Description("Автоматически выбирать SCP-343 при старте раунда")]
-        public bool EnableAutoSpawn { get; set; } = true;
-
-        [Description("Роль, из которой выбирается SCP-343")]
-        public RoleTypeId SpawnRole { get; set; } = RoleTypeId.ClassD;
-
-        [Description("Шанс спавна SCP-343 (0.0 - 1.0)")]
-        public float SpawnChance { get; set; } = 1.0f;
-
-        [Description("Минимальное количество игроков для спавна SCP-343")]
+        [Description("Минимальное количество игроков на сервере для появления SCP-343.")]
         public int MinPlayers { get; set; } = 1;
 
-        [Description("Количество аптечек, выдаваемых при спавне")]
+        [Description("Шанс появления SCP-343 (0.0 - 1.0).")]
+        public float SpawnChance { get; set; } = 1.0f;
+
+        [Description("Включить автоспавн SCP-343 в начале раунда?")]
+        public bool EnableAutoSpawn { get; set; } = true;
+
+        [Description("Количество аптечек при спавне SCP-343.")]
         public int MedkitCount { get; set; } = 8;
 
-        [Description("Включить bypass для SCP-343")]
+        [Description("Выдавать ли обход режиму (noclip) SCP-343?")]
         public bool EnableBypass { get; set; } = true;
 
-        [Description("Включить godmode для SCP-343")]
+        [Description("Выдавать ли SCP-343 бессмертие?")]
         public bool EnableGodMode { get; set; } = true;
 
-        [Description("Во что превращать оружие (по умолчанию SCP500)")]
-        public ItemType WeaponConvertTo { get; set; } = ItemType.SCP500;
+        [Description("В какое оружие конвертировать поднятое оружие?")]
+        public ItemType WeaponConvertTo { get; set; } = ItemType.GunCOM15;
 
-        [Description("Сообщение при спавне SCP-343")]
+        [Description("Текст хинта при спавне SCP-343.")]
         public string SpawnHint { get; set; } =
-            "<color=yellow><b>SCP-343 (БОГ)</b></color>\n" +
-            "<color=grey>Все предметы, которые ты подбираешь, становятся аптечками.</color>\n" +
-            "<color=grey>Всё оружие превращается в SCP-500.</color>\n" +
-            "<color=#00bfff>Ты получаешь 8 аптечек, bypass и godmode при спавне.</color>\n" +
-            "<color=yellow>CustomInfo: scp343</color>";
+    "<b><color=white>Сбегите из комплекса.</color>\n" +
+    "<color=#00FF00>Объединитесь с Повстанцами Хаоса.</color>\n" +
+    "<color=white>Избегайте других выживших, а также <color=red>объектов SCP.</color></color></b>\n\n" +
+    "<color=#00BFFF>Ты получаешь 8 аптечек, BYPASS и GODMODE при спавне.</color>\n" +
+    "<color=yellow>CustomInfo: SCP343</color>";
+
+        [Description("Текст хинта при получении новой аптечки.")]
+        public string MedkitHint { get; set; } =
+            "<color=#FFD700>Ты всегда получаешь новую аптечку!</color>";
+
+        [Description("Текст хинта при попытке поднять SCP-предмет или МикроХИД.")]
+        public string ScpItemBlockHint { get; set; } =
+            "<color=red>SCP-343 не может поднимать SCP-предметы и МикроХИД!</color>";
     }
 }
